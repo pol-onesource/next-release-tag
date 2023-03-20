7861748e8bab979f3b1995ae45c4b0d3ca894107
Aspen <aspen.thompson@postoffice.co.uk>
2023-03-20 16:20:03 +0000

    Iterate over tags, ignoring non-automated releases

--- src/services/githubService.ts
+++ src/services/githubService.ts
@@ -6,11 +6,24 @@ export const fetchLatestReleaseTag = async () => {
     const githubToken = getInput('github_token', { required: true });
     const octokit = getOctokit(githubToken);
     const { owner, repo } = context.repo;
-    const response = await octokit.rest.repos.getLatestRelease({
-      owner,
-      repo,
-    });
-    return response.data.tag_name;
+
+    const releasesPageIterator = octokit.paginate.iterator(
+      octokit.rest.repos.listReleases,
+      {
+        owner,
+        repo,
+      }
+    );
+
+    for await (const { data: releases } of releasesPageIterator) {
+      for (const { tag_name: tagName } of releases) {
+        if (/^v\d{2}.*/.test(tagName)) {
+          return tagName;
+        }
+      }
+    }
+
+    return null;
   } catch (error: any) {
     // No releases yet
     if (error?.response?.status === 404) {
